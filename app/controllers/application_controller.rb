4b0e606fe5b1c2addc81cfcbc4f3f7a1edb7d42a
hagiohagi <hagigyyyyyyy@gmail.com>
2020-08-14 18:47:00 +0900

    ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname,:firstname_kanji,:lastname_kanji,:firstname_katakana,:lastname_katakana,:birth_day])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+    validates :nickname, presence: true
+    validates :email,              presence: true
+    validates :password, presence: true, length:{ minimum:6 }, format: { with: (/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i) }
+    validates :firstname_kanji, presence: true, format: { with: (/\A[ぁ-んァ-ン一-龥]+\z/) }
+    validates :lastname_kanji, presence: true, format: { with:(/\A[ぁ-んァ-ン一-龥]+\z/) }
+    validates :firstname_katakana, presence: true, format: { with:(/\A[ァ-ヶー－]+\z/) }
+    validates :lastname_katakana, presence: true, format: { with:(/\A[ァ-ヶー－]+\z/)}
+    validates :birth_day, presence: true
 end
