49cc19fb8b702634583c6096dc0fa463a9f5357d
hagiohagi <hagigyyyyyyy@gmail.com>
2020-08-20 15:58:30 +0900

    商品出品機能の実装２

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,13 @@ class Item < ApplicationRecord
   validates :name, :image, :price, :text, :genre_id, :quality_id, :payment_id, :prefecture_id, :days_id, presence: true
   validates :price, numericality: {less_than: 9999999,greater_than: 300}
   
-  validates :genre_id, numericality: { other_than: 0 }
-  validates :quality_id, numericality: { other_than: 0 }
-  validates :payment_id, numericality: { other_than: 0 }
-  validates :prefecture_id, numericality: { other_than: 0 }
-  validates :days_id, numericality: { other_than: 0 }
+
+  with_options numericality: { other_than: 0 }
+  validates :genre_id
+  validates :quality_id
+  validates :payment_id
+  validates :prefecture_id
+  validates :days_id
 
   belongs_to :user
   has_many :comments
