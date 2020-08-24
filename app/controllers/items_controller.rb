634fd659cf8bd110b8488073ee2b93c86b9552d8
hagiohagi <hagigyyyyyyy@gmail.com>
2020-08-24 20:35:57 +0900

    商品情報編集機能の実装修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,10 +27,16 @@ class ItemsController < ApplicationController
   end
 
   def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-    item = Tweet.find(params[:id])
+    item = Item.find(params[:id])
     item.destroy
   end
 
