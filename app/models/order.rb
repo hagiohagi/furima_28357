7ed9aafcc1b002b1bcbe3403435d401e7ce226c2
hagiohagi <hagigyyyyyyy@gmail.com>
2020-08-28 23:04:45 +0900

    dependantメソッドの実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,13 @@
 class Order < ApplicationRecord
+<<<<<<< Updated upstream
 
 belongs_to :user
 belongs_to :item
 has_one :address
 
+=======
+  belongs_to :user
+  belongs_to :item
+  has_one :address, dependent: :destroy
+>>>>>>> Stashed changes
 end
