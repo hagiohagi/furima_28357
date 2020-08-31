fc07554ddd683bf5580442bf9585d839e4d54ce1
hagiohagi <hagigyyyyyyy@gmail.com>
2020-08-31 17:41:38 +0900

    トークン保存の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,6 +13,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @user = User.find(@item.user_id)
     @order = OrderAddress.new(order_params)
+    binding.pry
     if @order.valid?
       pay_item
       @order.save
@@ -32,7 +33,7 @@ class OrdersController < ApplicationController
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
       amount: @item.price, # 商品の値段
-      card: params[:token], # カードトークン
+      card: order_params[:token], # カードトークン
       currency: 'jpy'                 # 通貨の種類(日本円)
     )
   end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,24 +1,28 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  // console.log("Ok")
   const form = document.getElementById("charge-form");
+  // console.log(form)
+
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-
+    console.log("aaaa")
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
-    console.log(formData)
     const card = {
       number: formData.get("order_address[number]"),
       cvc: formData.get("order_address[cvc]"),
       exp_month: formData.get("order_address[exp_month]"),
       exp_year: `20${formData.get("order_address[exp_year]")}`,
     };
+    console.log(card)
     Payjp.createToken(card, (status, response) => {
+      console.log(status)
       if (status === 200) {
         console.log(response)
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        const tokenObj = `<input value=${token} type="text" name='order_address[token]'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
         console.log(tokenObj)
         document.getElementById("number").removeAttribute("name");
