8f685d3f7b2759ac5384bc329e2a1487e1ccea22
hagiohagi <hagigyyyyyyy@gmail.com>
2020-09-26 16:27:11 +0900

    robocop稼働による微調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,9 @@ class ApplicationController < ActionController::Base
 
   private
 
-  private
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
     end
   end
 end
- 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,9 @@
 class Order < ApplicationRecord
-
-
-belongs_to :user
-belongs_to :item
-has_one :address
-
+  belongs_to :user
+  belongs_to :item
+  has_one :address
 
   belongs_to :user
   belongs_to :item
   has_one :address, dependent: :destroy
-
 end
